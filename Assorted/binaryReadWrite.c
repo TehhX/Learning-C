978e6cb721f81a46d888a729820bf9f3590bac90
Sam <156055395+TehhX@users.noreply.github.com>
2025-06-28 23:36:24 -0400

    Added string example to binaryReadWrite.

--- Assorted/binaryReadWrite.c
+++ Assorted/binaryReadWrite.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define TYPE_ONE unsigned char
 #define TYPE_TWO int
 #define TYPE_THREE unsigned long long
+#define TYPE_FOUR signed char
 
 #define F_SPEC_ONE "%d"
 #define F_SPEC_TWO "%d"
 #define F_SPEC_THREE "%llu"
+#define F_SPEC_FOUR "%s"
 
 #define SIZE_ONE 1
 #define SIZE_TWO sizeof(int)
 #define SIZE_THREE sizeof(unsigned long long)
+#define SIZE_FOUR 14
 
 #define VALUE_ONE 255
 #define VALUE_TWO 29586
 #define VALUE_THREE 123123123123123
+#define VALUE_FOUR "Hello, world."
 
 void write() {
     FILE* out = fopen("myBinaryFile.bin", "wb");
@@ -34,8 +39,11 @@ void write() {
     TYPE_THREE valThree = VALUE_THREE;
     fwrite(&valThree, SIZE_THREE, 1, out);
 
+    TYPE_FOUR valFour[] = VALUE_FOUR;
+    fputs(valFour, out);
+
     if (fclose(out)) {
-        perror("Could not close writing file.\n");
+        printf("Could not close writing file.\n");
         exit(1);
     }
 }
@@ -44,7 +52,7 @@ void read() {
     FILE* in = fopen("myBinaryFile.bin", "rb");
 
     if (in == NULL) {
-        perror("Could not open reading file.\n");
+        printf("Could not open reading file.\n");
         exit(1);
     }
 
@@ -52,22 +60,28 @@ void read() {
     fread(&valOne, SIZE_ONE, 1, in);
     printf("valOne: " F_SPEC_ONE "\n", valOne);
     if (valOne != VALUE_ONE)
-        perror("valOne does not match.\n");
+        printf("valOne does not match.\n");
     
     TYPE_TWO valTwo;
     fread(&valTwo, SIZE_TWO, 1, in);
     printf("valTwo: " F_SPEC_TWO "\n", valTwo);
     if (valTwo != VALUE_TWO)
-        perror("valTwo does not match.\n");
+        printf("valTwo does not match.\n");
 
     TYPE_THREE valThree;
     fread(&valThree, SIZE_THREE, 1, in);
     printf("valThree: " F_SPEC_THREE "\n", valThree);
     if (valThree != VALUE_THREE)
-        perror("valThree does not match.\n");
-    
+        printf("valThree does not match.\n");
+
+    TYPE_FOUR valFour[SIZE_FOUR];
+    fgets(valFour, SIZE_FOUR, in);
+    printf("valFour: " F_SPEC_FOUR "\n", valFour);
+    if (strcmp(valFour, VALUE_FOUR))
+        printf("valFour does not match.\n");
+
     if (fclose(in)) {
-        perror("Could not close reading file.\n");
+        printf("Could not close reading file.\n");
         exit(1);
     }
 }
