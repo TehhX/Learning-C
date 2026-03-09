883ca41a64c939b772bb8af17b26338f970d5f83
TehhX <156055395+TehhX@users.noreply.github.com>
2026-03-08 21:38:12 -0400

    Updated functionality of Assorted/slider_to_rgb.c.

--- Assorted/slider_to_rgb.c
+++ Assorted/slider_to_rgb.c
@@ -20,24 +20,52 @@ struct color color_from_slider(float slider)
 
 void color_print(struct color c)
 {
-    printf("RGB( %3d, %3d, %3d )\n", c.r, c.g, c.b);
+    printf("%3d, %3d, %3d,\n", c.r, c.g, c.b);
 }
 
-#define STEPS 10
+const char help_text[] =
+    "| Arg Position |                          Expectation                           | Example |\n"
+    "|--------------|----------------------------------------------------------------|---------|\n"
+    "|      0       | Either 's' or 'v'. 's' for steps, 'v' for single slider value. |    s    |\n"
+    "|   1 (0=v)    |                        Value of slider.                        |  0.25   |\n"
+    "|   1 (0=s)    |                        Amount of steps                         |   100   |"
+;
 
 int main(int argc, char **argv)
 {
-    // No arguments, go through steps
-    if (argc == 1)
+    // Get rid of 1st argument
+    --argc; ++argv;
+
+    if (argc == 0)
     {
-        for (float i = 0; i <= 1.0; i += 1.0f / (STEPS - 1))
+        puts(help_text);
+        return 0;
+    }
+    else if (argv[0][0] == 'v')
+    {
+        const float value = atof(argv[1]);
+
+        if (value > 1.0f || value < 0.0f)
+        {
+            puts("Value of slider must be 0.0 <= s <= 1.0.");
+            return 1;
+        }
+
+        color_print(color_from_slider(value));
+    }
+    else if (argv[0][0] == 's')
+    {
+        const int steps = atol(argv[1]);
+
+        for (float i = 0; i <= 1.0; i += 1.0f / (steps - 1))
         {
             color_print(color_from_slider(i));
         }
     }
-    // Take argument as slider value
     else
     {
-        color_print(color_from_slider(atof(argv[1])));
+        puts("Bad value at argv index 1. Review help:");
+        puts(help_text);
+        return 1;
     }
 }
