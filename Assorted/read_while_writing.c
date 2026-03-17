d2f100138b69e1187f967bc2dd395bbc961752b7
TehhX <156055395+TehhX@users.noreply.github.com>
2026-03-17 19:40:31 -0400

    read_while_writing now uses buffer, other related changes.

--- Assorted/read_while_writing.c
+++ Assorted/read_while_writing.c
@@ -1,36 +1,44 @@
-// This program is meant to read from a file which is currently being written to by another process. Instructions taken from https://stackoverflow.com/a/44351438.
+// This program is meant to read from a file which is currently being written to by another process. Instructions taken from https://stackoverflow.com/a/44351438 and https://stackoverflow.com/a/15485689.
 
 #include "stdio.h"
 #include "stdlib.h"
 #include "unistd.h"
 
-#define STEP_SECONDS 5
+#define STEP_SECONDS    (int)   1 // Seconds before rescans
+#define BUFFER_BYTES (size_t) 128 // Bytes in the buffer
 
 int main(int argc, char **argv)
 {
-    if (argc != 2)
+    // 0th argument not required
+    --argc; ++argv;
+
+    // First argument should have file name
+    if (argc != 1)
     {
         puts("Need file argument.");
         return 1;
     }
 
-    FILE *const input_file = fopen(argv[1], "r");
+    // Standard file open
+    FILE *const input_file = fopen(argv[0], "r");
     if (!input_file)
     {
-        fprintf(stderr, "Failed to open file '%s' for reading: ", argv[1]);
+        fprintf(stderr, "Failed to open file '%s' for reading: ", argv[0]);
         perror("");
         return 1;
     }
 
     do
     {
-        // TODO: Buffer characters more than 1 at a time
-        for (int c; (c = getc(input_file)) != EOF;)
+        // fgets returns NULL if no characters are returned at EOF
+        for (char buffer[BUFFER_BYTES]; NULL != fgets(buffer, BUFFER_BYTES, input_file) ; )
         {
-            putchar(c);
+            printf(buffer);
         }
 
+        // Clears the EOF of input_file. Not entirely sure why it's required given my modifcations, unless fgets returns NULL if EOF is set regardless of stream content
         clearerr(input_file);
     }
-    while (!sleep(STEP_SECONDS));
+    // If STEP_SECONDS > 0, sleep while it returns no errors. If 0, do without sleeping forever
+    while (STEP_SECONDS > 0 ? !sleep(STEP_SECONDS) : 1);
 }
