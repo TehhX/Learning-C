89eb4dcddb5472ceb2542708831b43877a1d7944
TehhX <samuel.tobias@tehx.ca>
2026-04-18 19:51:50 -0400

    Modified sigint_interception.c for windows support

--- Assorted/sigint_interception.c
+++ Assorted/sigint_interception.c
@@ -2,18 +2,38 @@
 
 #include "stdio.h"
 #include "stdbool.h"
-#include "signal.h"
 
 static bool sigint_caught = false;
 
-void sigint_handler(int _)
-{
-    sigint_caught = true;
-}
+#ifdef __linux__
+    #include "signal.h"
+
+    void sigint_handler(int _)
+    {
+        sigint_caught = true;
+    }
+#elif defined(_WIN32) || defined(_WIN64)
+    #include "windows.h"
+
+    BOOL sigint_handler(DWORD _)
+    {
+        sigint_caught = true;
+
+        return TRUE;
+    }
+#else
+    #error "Unknown OS."
+#endif
 
 int main()
 {
+#ifdef __linux__
     if (signal(SIGINT, sigint_handler) == SIG_ERR)
+#elif defined(_WIN32) || defined(_WIN64)
+    if (!SetConsoleCtrlHandler(sigint_handler, TRUE))
+#else
+    #error "Unknown OS."
+#endif
     {
         perror("Couldn't change signal");
         return 1;
