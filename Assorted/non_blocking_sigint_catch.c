d2eb4327b184136a38e509907eca35838f98d25f
TehhX <samuel.tobias@tehx.ca>
2026-04-20 02:24:48 -0400

    NBSC now has basic windows implementation, not tested

--- Assorted/non_blocking_sigint_catch.c
+++ Assorted/non_blocking_sigint_catch.c
@@ -1,35 +1,91 @@
-// Get user input without blocking until they type
-// Currently Linux only
+// Get user input without blocking until they type. Foundational info in /Assorted/sigint_interception.c
 
 #include "stdio.h"
-#include "unistd.h"
 #include "stdbool.h"
-#include "poll.h"
-#include "signal.h"
 #include "string.h"
 
+#define NBSC_PLAT_LIN 0
+#define NBSC_PLAT_WIN 1
+
 static bool caught = false;
-void sigint_handler(int _)
-{
-    caught = true;
-}
+
+#ifdef __linux__
+    #define NBSC_PLAT NBSC_PLAT_LIN
+    #define NBSC_INSERT_LIN(CONTENTS) CONTENTS
+    #define NBSC_INSERT_WIN(CONTENTS)
+
+    #include "unistd.h"
+    #include "poll.h"
+    #include "signal.h"
+    #include "errno.h"
+
+    #define FD_STDIN STDIN_FILENO
+    #define READABLE_FLAG POLLIN
+    #define POLL_FUNC poll
+    #define POLL_ERROR -1
+
+    void sigint_handler(int _)
+    {
+        caught = true;
+    }
+#elif defined(_WIN32)
+    #define NBSC_PLAT NBSC_PLAT_WIN
+    #define NBSC_INSERT_LIN(CONTENTS)
+    #define NBSC_INSERT_WIN(CONTENTS) CONTENTS
+
+    #include "windows.h"
+
+    #define FD_STDIN 0
+    #define READABLE_FLAG POLLRDNORM
+    #define POLL_FUNC WSAPoll
+    #define POLL_ERROR SOCKET_ERROR
+
+    BOOL sigint_handler(DWORD _)
+    {
+        caught = true;
+
+        return TRUE;
+    }
+#else
+    #error "Unknown OS."
+#endif
 
 int main()
 {
-    signal(SIGINT, sigint_handler);
+    if
+    (
+        NBSC_INSERT_LIN(signal(SIGINT, sigint_handler) == SIG_ERR)
+        NBSC_INSERT_WIN(!SetConsoleCtrlHandler(sigint_handler, TRUE))
+    )
+    {
+        perror("Couldn't modify SIGINT handling");
+        return 1;
+    }
 
     while (!caught)
     {
         struct pollfd poll_input =
         {
-            .fd = STDIN_FILENO,
-            .events = POLLPRI | POLLIN,
-            .revents = 0
+            .fd = FD_STDIN,
+            .events = READABLE_FLAG
         };
 
-        poll(&poll_input, 1, 0);
+        if (POLL_FUNC(&poll_input, 1, 0) == POLL_ERROR)
+        {
+            // Interrupted by CTRL+C while polling, leave loop
+            if (errno == EINTR)
+            {
+                break;
+            }
+            // Unknown error, exit failure
+            else
+            {
+                perror("Poll failed with code other than EINTR");
+                return 1;
+            }
+        }
 
-        if ((poll_input.revents & POLLIN) && !(poll_input.revents & POLLPRI))
+        if (poll_input.revents & READABLE_FLAG)
         {
             char buf[256];
             fgets(buf, 256, stdin);
@@ -39,5 +95,13 @@ int main()
 
     printf("\nCaught SIGINT, exiting.\n");
 
-    signal(SIGINT, NULL);
+    if
+    (
+        NBSC_INSERT_LIN(signal(SIGINT, NULL) == SIG_ERR)
+        NBSC_INSERT_WIN(!SetConsoleCtrlHandler(NULL, FALSE))
+    )
+    {
+        perror("Couldn't restore SIGINT handling");
+        return 1;
+    }
 }
