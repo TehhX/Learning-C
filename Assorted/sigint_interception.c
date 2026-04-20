2f792f807962e5a55b50c8985bd4541de0a03276
TehhX <samuel.tobias@tehx.ca>
2026-04-20 01:43:59 -0400

    Updated sigint_intreception.c with clean-up.

--- Assorted/sigint_interception.c
+++ Assorted/sigint_interception.c
@@ -1,18 +1,33 @@
 // Intercepts SIGINT, runs custom action instead of default process exit. Will catch it 3 times before actually exiting
 
+#define SI_PLAT_LIN 0
+#define SI_PLAT_WIN 1
+
+#ifdef __linux__
+    #define SI_PLAT SI_PLAT_LIN
+    #define SI_INSERT_LIN(CONTENTS) CONTENTS
+    #define SI_INSERT_WIN(CONTENTS)
+#elif defined(_WIN32)
+    #define SI_PLAT SI_PLAT_WIN
+    #define SI_INSERT_LIN(CONTENTS)
+    #define SI_INSERT_WIN(CONTENTS) CONTENTS
+#else
+    #error "Unknown OS."
+#endif
+
 #include "stdio.h"
 #include "stdbool.h"
 
 static bool sigint_caught = false;
 
-#ifdef __linux__
+#if SI_PLAT == SI_PLAT_LIN
     #include "signal.h"
 
     void sigint_handler(int _)
     {
         sigint_caught = true;
     }
-#elif defined(_WIN32) || defined(_WIN64)
+#elif SI_PLAT == SI_PLAT_WIN
     #include "windows.h"
 
     BOOL WINAPI sigint_handler(DWORD _)
@@ -21,21 +36,19 @@ static bool sigint_caught = false;
 
         return TRUE;
     }
-#else
-    #error "Unknown OS."
 #endif
 
 int main()
 {
-#ifdef __linux__
-    if (signal(SIGINT, sigint_handler) == SIG_ERR)
-#elif defined(_WIN32) || defined(_WIN64)
-    if (!SetConsoleCtrlHandler(sigint_handler, TRUE))
-#else
-    #error "Unknown OS."
-#endif
+    // Set up catching
+    if
+    (
+        SI_INSERT_LIN(signal(SIGINT, sigint_handler) == SIG_ERR)
+        SI_INSERT_WIN(!SetConsoleCtrlHandler(sigint_handler, TRUE))
+    )
+    // Will execute block if failed
     {
-        perror("Couldn't change signal");
+        perror("Couldn't modify SIGINT handling");
         return 1;
     }
 
@@ -46,5 +59,16 @@ int main()
             printf("Caught SIGINT %d times.\n", ++times_caught);
             sigint_caught = false;
         }
+    }
+
+    // Reset terminal SIGINT catching
+    if
+    (
+        SI_INSERT_LIN(signal(SIGINT, NULL) == SIG_ERR)
+        SI_INSERT_WIN(!SetConsoleCtrlHandler(NULL, FALSE))
+    )
+    {
+        perror("Couldn't restore SIGINT handling");
+        return 1;
     }
 }
