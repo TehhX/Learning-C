fc01c968a9ab4946296b3042dfeb9b2723bae42d
TehhX <samuel.tobias@tehx.ca>
2026-04-21 21:08:16 -0700

    win32_key_pressed.c now also makes sure console window is in foreground

--- Assorted/win32_key_pressed.c
+++ Assorted/win32_key_pressed.c
@@ -9,10 +9,21 @@ int main()
     // Get rid of first garbage
     GetAsyncKeyState(VK_RETURN);
 
+    // Change title of window to W32KP (<PROCESS_ID>)
+    char new_title[16] = { 0 };
+    sprintf(new_title, "W32KP (%d)", GetCurrentProcessId());
+    SetConsoleTitleA(new_title);
+
+    // Wait for change to take
+    Sleep(5);
+
+    // Get HWND of console window via new title
+    const HWND console_window_handle = FindWindowA(NULL, new_title);
+
     for (unsigned long long count = 1; count; ++count)
     {
-	const SHORT key_state = GetAsyncKeyState(VK_RETURN);
-        if (key_state & 0x1)
+        // Check that console window is in foreground, and subsequently that enter (newline) key is pressed, if so print count
+        if (GetForegroundWindow() == console_window_handle && GetAsyncKeyState(VK_RETURN))
         {
             printf("Pressed on %llu.\n", count);
         }
