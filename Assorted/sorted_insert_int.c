e745c8466b20db9ea687aa95e73ee3ca262fe52c
TehhX <samuel.tobias@tehx.ca>
2026-07-21 12:57:29 -0400

    Kept working on sorted_insert

--- Assorted/sorted_insert_int.c
+++ Assorted/sorted_insert_int.c
@@ -127,6 +127,80 @@ void sorted_find_insert_int_binary(int **const array, size_t *const array_len, c
     on_insert((*array) + low, value);
 }
 
+// Searches using binary search
+void sorted_find_insert_int_binary_v2(int **const array, size_t *const array_len, const int value, sorted_find_insert_int_action_t on_find, sorted_find_insert_int_action_t on_insert)
+{
+    // TODO: Simplify edge cases
+    if (*array_len == 0)
+    {
+        *(*array = malloc(sizeof(int))) = value;
+        on_insert(*array, value);
+        ++*array_len;
+        return;
+    }
+    else if (array[0][0] > value)
+    {
+        *array = realloc(*array, sizeof(int) * ++*array_len);
+        memmove(array[0] + 1, array[0], sizeof(int) * (*array_len - 1));
+        on_insert(*array, value);
+        return;
+    }
+    else if (array[0][0] == value)
+    {
+        on_find(*array, value);
+        return;
+    }
+    else if (array[0][*array_len - 1] < value)
+    {
+        *array = realloc(*array, sizeof(int) * ++*array_len);
+        on_insert(*array + *array_len - 1, value);
+        return;
+    }
+    else if (array[0][*array_len - 1] == value)
+    {
+        on_find(*array + *array_len - 1, value);
+        return;
+    }
+    else if (*array_len == 1)
+    {
+        if (array[0][0] == value)
+        {
+            on_find(*array, value);
+            return;
+        }
+        else // Implicitly array[0][0] < value
+        {
+            *array = realloc(*array, sizeof(int) * ++*array_len);
+            on_insert((*array) + 1, value);
+            return;
+        }
+    }
+
+    size_t low = 0, high = *array_len - 1;
+    while (low < high)
+    {
+        const size_t mid = low + (high - low) / 2;
+        if (array[0][mid] == value)
+        {
+            on_find(array[0] + mid, value);
+            return;
+        }
+        else if (array[0][mid] < value)
+        {
+            low = mid + 1;
+        }
+        else // Implicitly array[0][mid] > value
+        {
+            high = mid - 1;
+        }
+    }
+
+    // Didn't find value
+    *array = realloc(*array, sizeof(int) * ++*array_len);
+    memmove((*array) + low, (*array) + high, sizeof(int) * (*array_len - low));
+    on_insert((*array) + low, value);
+}
+
 const int *element;
 
 void list_on_find(int *const list_element, __attribute__((unused)) const int value)
@@ -153,12 +227,10 @@ static inline void test_method(const char *const identifier, sorted_find_insert_
 
     method(&list, &list_len, value, list_on_find, list_on_insert);
 
-    const int correct_index = (expected_index == (size_t) (element - list));
-
     struct timeval end_tval;
     gettimeofday(&end_tval, NULL);
 
-    const double total = (end_tval.tv_usec / 1000000.0 + end_tval.tv_sec) - (start_tval.tv_usec / 1000000.0 + start_tval.tv_sec);
+    const int correct_index = (expected_index == (size_t) (element - list));
 
     int correct_contents = 1;
     if (list_len == expected_result_len)
@@ -179,7 +251,7 @@ static inline void test_method(const char *const identifier, sorted_find_insert_
 
     free(list);
 
-    printf("%20s | %8lfs | %15s | %12s\n", identifier, total, (correct_contents ? "Correct" : "  False"), (correct_index ? "Correct" : "  False"));
+    printf("%20s | %8lfs | %15s | %12s\n", identifier, (end_tval.tv_usec / 1000000.0 + end_tval.tv_sec) - (start_tval.tv_usec / 1000000.0 + start_tval.tv_sec), (correct_contents ? "Correct" : "  False"), (correct_index ? "Correct" : "  False"));
 }
 
 void test_cases(const char *const method_identifier, sorted_find_insert_int method)
@@ -192,17 +264,19 @@ void test_cases(const char *const method_identifier, sorted_find_insert_int meth
         method_identifier
     );
 
-    test_method(   "Find Start", method, 4,   0, (int[]){ 0, 4, 8, 12 },      4, 0);
-    test_method(  "Find Middle", method, 4,   8, (int[]){ 0, 4, 8, 12 },      4, 2);
-    test_method(     "Find End", method, 4,  12, (int[]){ 0, 4, 8, 12 },      4, 3);
-    test_method(   "Insert Pre", method, 4, -10, (int[]){ -10, 0, 4, 8, 12 }, 5, 0);
-    test_method("Insert Middle", method, 4,   5, (int[]){ 0, 4, 5, 8, 12 },   5, 2);
-    test_method(  "Insert Post", method, 4,  13, (int[]){ 0, 4, 8, 12, 13 },  5, 4);
-    test_method(        "Empty", method, 0,  32, (int[]){ 32 },               1, 0);
+    test_method(        "Find Start", method, 4,   0, (int[]){ 0, 4, 8, 12 },      4, 0);
+    test_method(       "Find Middle", method, 4,   8, (int[]){ 0, 4, 8, 12 },      4, 2);
+    test_method(          "Find End", method, 4,  12, (int[]){ 0, 4, 8, 12 },      4, 3);
+    test_method(        "Insert Pre", method, 4, -10, (int[]){ -10, 0, 4, 8, 12 }, 5, 0);
+    test_method( "Insert Middle Odd", method, 4,   5, (int[]){ 0, 4, 5, 8, 12 },   5, 2);
+    test_method("Insert Middle Even", method, 4,   6, (int[]){ 0, 4, 6, 8, 12 },   5, 2);
+    test_method(       "Insert Post", method, 4,  13, (int[]){ 0, 4, 8, 12, 13 },  5, 4);
+    test_method(             "Empty", method, 0,  32, (int[]){ 32 },               1, 0);
 }
 
 int main()
 {
-    test_cases("Linear", sorted_find_insert_int_linear);
-    test_cases("Binary", sorted_find_insert_int_binary);
+    test_cases(   "Linear", sorted_find_insert_int_linear);
+    test_cases(   "Binary", sorted_find_insert_int_binary);
+    test_cases("Binary V2", sorted_find_insert_int_binary_v2);
 }
